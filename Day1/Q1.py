27f69013749c6a25f1eb0c3344fd50066eecf7e1
Monkiia <chenshensheng888@gmail.com>
2024-10-09 15:30:46 +0800

    Q1 POW implementation

---
+++ Day1/Q1.py
@@ -0,0 +1,24 @@
+import time
+import hashlib
+## print that we are going to get 4 zeros
+## start time
+
+def calculate_time(expected_input):
+    # print expected input
+    print("Expected input top Hash: ", expected_input)
+    start = time.time()
+    username = "sschenx"
+    nounce = 0
+    # sha256
+    while True:
+        nounce += 1
+        data = username + str(nounce)
+        hash_value = hashlib.sha256(data.encode()).hexdigest()
+        if hash_value[:len(expected_input)] == expected_input:
+            break
+    print("Nounce: ", nounce)
+    print("Hash: ", hash_value)
+    print("Time: ", time.time() - start)
+
+calculate_time("0000")
+calculate_time("00000")
